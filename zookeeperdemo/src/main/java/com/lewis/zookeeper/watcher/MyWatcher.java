6a40fe8e9bc53e19df312a6c90c5478143c6474a
arrowToTarget <1017635978@qq.com>
2016-04-12 14:28:58 +0800

    Initial commit

---
+++ zookeeperdemo/src/main/java/com/lewis/zookeeper/watcher/MyWatcher.java
@@ -0,0 +1,14 @@
+package com.lewis.zookeeper.watcher;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+/**
+ * Created by zhangminghua on 2016/4/12.
+ */
+public class MyWatcher implements Watcher{
+
+    public void process(WatchedEvent watchedEvent) {
+
+    }
+}
---
+++ zookeeperdemo/src/main/java/com/lewis/zookeeper/watcher/WatcherTest.java
@@ -0,0 +1,11 @@
+package com.lewis.zookeeper.watcher;
+
+/**
+ * Created by zhangminghua on 2016/4/12.
+ */
+public class WatcherTest {
+
+    public static void main(String[] args) {
+
+    }
+}
